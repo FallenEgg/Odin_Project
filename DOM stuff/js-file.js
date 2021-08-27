40acf397643ffbe4a3e83d77d46b4d78e1476ff9
Eddie Bailey <60523317+FallenEgg@users.noreply.github.com>
2021-08-27 12:53:08 -0400

    DOM test
    HTML and js file for DOM manipulation

---
+++ DOM stuff/js-file.js
@@ -0,0 +1,23 @@
+const p = document.createElement('p');
+p.style.color = 'red'
+p.textContent = "Hey I'm Red!"
+
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = "I'm a blue h3"
+
+const h1 = document.createElement('h1');
+h1.textContent = "I'm in a div"
+
+const p1 = document.createElement('p');
+p1.textContent = "ME TOO"
+
+const div = document.createElement('div');
+div.appendChild(h1, p1);
+div.style.cssText = 'color: blue; background: white';        
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
